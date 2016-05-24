eeef54e2577af035e4fede6a75611ae10323728b
Kagamine <1@1234.sh>
2016-05-24 23:57:58 +0800

    Update Program.cs

--- src/$safeprojectname$/Program.cs
+++ src/$safeprojectname$/Program.cs
@@ -13,6 +13,7 @@ namespace $safeprojectname$
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://*:5000")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
